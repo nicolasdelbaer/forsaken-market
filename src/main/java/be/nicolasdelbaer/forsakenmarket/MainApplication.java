dc659651bff191097cecff96f3498c0658cac354
nicolasdelbaer <n.delbaer@gmail.com>
2026-04-26 12:45:29 +0200

    chore: remove .idea & .mvn from tracking

---
+++ src/main/java/be/nicolasdelbaer/forsakenmarket/MainApplication.java
@@ -0,0 +1,9 @@
+package be.nicolasdelbaer.forsakenmarket;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
