e0a8e14f2303d33f8e8a30a1afab63b571717647
nicolasdelbaer <n.delbaer@gmail.com>
2026-04-30 13:55:06 +0200

    fix -reputation calculator zerodiv

--- src/main/java/be/nicolasdelbaer/forsakenmarket/utils/ReputationCalculator.java
+++ src/main/java/be/nicolasdelbaer/forsakenmarket/utils/ReputationCalculator.java
@@ -6,6 +6,9 @@ public class ReputationCalculator {
     public static Integer calculate(ReputationScoreData data) {
         Integer boughtPrice = data.boughtItem().getBoughtPrice();
         Integer profit = boughtPrice - data.marketPrice().getCurrentPrice();
-        return Math.max(0, (int) (profit / boughtPrice * 10));
+        int result = 0;
+        if(boughtPrice != 0)
+            result =  Math.max(0, (int)((double) profit / boughtPrice * 10));
+        return result;
     }
 }
